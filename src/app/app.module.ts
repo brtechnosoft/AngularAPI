3e2f69d6032b8c109c36df92681be0778f6a34fb
vijayaprag <31339437+vijayaprag@users.noreply.github.com>
2019-03-13 16:23:38 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { UserComponent } from "./users/users.component";
+import { AlbumComponent } from "./album/album.component";
+import { AlbumComponent2 } from "./album/album2.component";
+import { AlbumDetailComponent } from "./album-detail/album-detail.component";
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,UserComponent,AlbumComponent,AlbumDetailComponent,AlbumComponent2
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	BrowserAnimationsModule,
+   HttpModule, FormsModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
