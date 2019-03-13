c81d54ab6abbda2aa19eaa979e6e8d6a4e08832b
vijayaprag <31339437+vijayaprag@users.noreply.github.com>
2019-03-13 16:23:16 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { map, filter, scan } from 'rxjs/operators';
+import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent, SubscriptionLike, PartialObserver } from 'rxjs';
+
+import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'task';
+ title = 'AngularUsers';
+
+
+  constructor(private http: Http) { }
+
+httpdata;
+   ngOnInit() {
+      this.http.get("http://jsonplaceholder.typicode.com/users")
+      .pipe(map((response) => response.json()))
+      .subscribe((data) => this.displaydata(data));     
+   }
+   displaydata(data) {this.httpdata = data;}
 }
